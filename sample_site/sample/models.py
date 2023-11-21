5de6160816a850590969fa90628e68b9fbfc8337
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2023-11-21 23:27:59 +1100

    Employee Roster
    Created Model and Added records

--- sample_site/sample/models.py
+++ sample_site/sample/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class EmployeeRoster(models.Model):
+    fName = models.CharField(max_length=255)
+    lName = models.CharField(max_length=255)
+    Department = models.CharField(max_length=255)
+    Supervisor = models.CharField(max_length=255)
