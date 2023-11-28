d8ee0a68b9774d6d45f5c4f259cb9279841a76d9
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2023-11-28 22:13:51 +1100

    English Worksheet
    Created English Worksheet 1 template and worksheet1 view

--- sample_site/sample/urls.py
+++ sample_site/sample/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name = "home"),
+    path('home/details/<int:id>', views.details, name = "details"),
+    path('eng/worksheet-1', views.worksheet1, name = "worksheet1")
 ]
--- sample_site/sample/views.py
+++ sample_site/sample/views.py
@@ -8,6 +8,7 @@ from .models import EmployeeRoster
 #     template = loader.get_template("myhome.html")
 #     return HttpResponse(template.render())
 
+# Home view
 def home(request):
     myEmployees = EmployeeRoster.objects.all().values()
     template = loader.get_template("myhome.html")
@@ -15,4 +16,17 @@ def home(request):
         "Employees":myEmployees
     }
     return HttpResponse(template.render(context, request))
-    
+
+# Member Details view
+def details(request, id):
+    myEmployee = EmployeeRoster.objects.get(id=id)
+    template = loader.get_template("member_details.html")
+    context = {
+        "member":myEmployee
+    }
+    return HttpResponse(template.render(context, request))
+
+# English Worksheet 1 view
+def worksheet1(request):
+    template = loader.get_template("eng/01-worksheet.html")
+    return HttpResponse(template.render())
