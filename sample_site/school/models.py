8fde65b730f0de96f75fa6783220aa2d5fa62137
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2024-02-25 00:26:12 +1100

    School app
    Add school model

--- sample_site/school/models.py
+++ sample_site/school/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# School Model
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=20)
+    yearOfEstablishment = models.PositiveIntegerField()
+    email = models.EmailField(max_length=200) 
+    phone = models.CharField(max_length=20)
+
